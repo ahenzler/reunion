95cd94650bbff6da124cd857b6b8afa74753e287
ahenzler <aliciahenzler@outlook.com>
2021-04-19 08:06:16 -0600

    iteration 3 in progress

--- lib/reunion.rb
+++ lib/reunion.rb
@@ -11,4 +11,16 @@ class Reunion
   def add_activity(activity)
     @activities << activity
   end
+
+  def total_cost
+    @activities.map do |activity|
+      activity.total_cost
+    end.sum
+  end
+
+  def breakout
+    owed_hashes = {}
+    @activities.each do |activity|
+      owed_hashes += activity.owed
+  end
 end
--- spec/reunion_spec.rb
+++ spec/reunion_spec.rb
@@ -19,10 +19,30 @@ RSpec.describe Reunion do
     reunion = Reunion.new("1406 BE")
     activity_1 = Activity.new("Brunch")
 
-    it 'can add activities' do
+    it 'can add an activity' do
       activity_1 = Activity.new("Brunch")
       reunion.add_activity(activity_1)
+      activity_1.add_participant("Maria", 20)
+      activity_1.add_participant("Luther", 40)
+
+
       expect(reunion.activities).to eq([activity_1])
+      expect(reunion.total_cost).to eq(60)
+    end
+
+    it 'can add another activity' do
+      activity_2 = Activity.new("Drinks")
+      activity_2.add_participant("Maria", 60)
+      activity_2.add_participant("Luther", 60)
+      activity_2.add_participant("Louis", 0)
+      reunion.add_activity(activity_2)
+
+      expect(reunion.total_cost).to eq(180)
+    end
+
+    it 'has a breakout method' do
+
+      expect(reunion.breakout).to eq({"Maria" => -10, "Luther" => -30, "Louis" => 40})
     end
   end
 end
